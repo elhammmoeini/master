47635ddfca8c9401b455227b5d76102d4821d653
Elhammoeini <elham.moeni@gmail.com>
2023-04-13 16:13:53 +0330

    master

--- classifier/tfkeras.py
+++ classifier/tfkeras.py
@@ -63,7 +63,7 @@ class GCAdam(Adam):
 def build_model():
     inputs = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
     x = inputs
-    model = EfficientNetV2S(weights=None, include_top=True, input_tensor=x, classes=2)
+    model = EfficientNetV2S(weights="./kd_effi_v2s.h5", include_top=True, input_tensor=x, classes=2)
     outputs = model.output
     model = tf.keras.Model(inputs, outputs, name="kd_effi_v2s")
     
@@ -77,8 +77,8 @@ callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, rest
 model = build_model()
 
 epochs = 50
-hist = model.fit(train_ds, epochs=epochs, validation_data=valid_ds, callbacks=[callback], verbose=1)
-model.save("kd_effi_v2s.h5")
+# hist = model.fit(train_ds, epochs=epochs, validation_data=valid_ds, callbacks=[callback], verbose=1)
+# model.save("kd_effi_v2s.h5")
 
 true_categories = []
 predicted_categories = []
